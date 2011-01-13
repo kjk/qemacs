7ae1cc5868ee1dd501410935ca17b8770974fe1e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2011-01-12 22:59:34 -0800

    remove unneded code

--- csharp/Page.cs
+++ csharp/Page.cs
@@ -89,12 +89,13 @@ public class Pages
     public const int MAX_PAGE_SIZE = 4096;
 
     public List<Page> page_table = new List<Page>();
-    public int nb_pages { get { return page_table.Count; } }
+    //public int nb_pages { get { return page_table.Count; } }
 
     /* page cache */
     public Page cur_page;
     public int cur_offset;
     public int cur_page_idx;
+
     public int total_size;
 
     public bool IsOffsetInCache(int offset)
@@ -378,22 +379,14 @@ public class EditBufferDataTypeRaw : IEditBufferDataType
 
 public class EditBuffer
 {
-    const int MAX_PAGE_SIZE = 4096;
-
     public List<IEditBufferDataType> DataTypes = new List<IEditBufferDataType>();
-    int total_size; /* total size of the buffer */
 
-    /* Page *page_table;
-    int nb_pages; */
-    List<Page> page_table = new List<Page>();
-    int nb_pages { get { return page_table.Count; } }
-
-    /* page cache */
-    Page cur_page = null;
-    int cur_offset;
+    Pages pages;
+    int TotalSize { get { return pages.total_size; } }
 
     public EditBuffer()
     {
+        pages = new Pages();
         RegisterDataType(new EditBufferDataTypeRaw());
     }
 
@@ -402,171 +395,11 @@ public class EditBuffer
         DataTypes.Add(dt);
     }
 
-    /* find a page at a given offset */
-    Page find_page(ref int offset_ptr)
-    {
-        Page p = null;
-        int offset;
-
-        offset = offset_ptr;
-        if ((cur_page != null) && (offset >= cur_offset) && 
-            (offset < cur_offset + cur_page.size)) {
-            /* use the cache */
-            offset_ptr -= cur_offset;
-            return cur_page;
-        } else {
-            int page_no = 0;
-            p = page_table[page_no];
-            while (offset >= p.size) {
-                offset -= p.size;
-                page_no += 1;
-                p = page_table[page_no];
-            }
-            cur_page = p;
-            cur_offset = offset_ptr - offset;
-            offset_ptr = offset;
-            return p;
-        }
-    }
-    
-    int PageIndex(Page p)
-    {
-        return page_table.IndexOf(p);
-    }
-
-    /* prepare a page to be written */
-    void update_page(Page p)
-    {
-        /* if the page is read only, copy it */
-        if (p.read_only) {
-            byte[] new_data = new byte[p.size];
-            Buffer.BlockCopy(p.data, 0, new_data, 0, p.size);
-            p.data = new_data;
-            p.read_only = false;
-        }
-
-        p.InvalidateAttributes();
-    }
-
-    /* internal function for insertion : 'buf' of size 'size' at the
-       beginning of the page at page_index */
-    void Insert1(int page_index, byte[] buf, int size)
-    {
-        int len, n;
-        Page p = null;
-
-        if (page_index < nb_pages) {
-            p = page_table[page_index];
-            len = MAX_PAGE_SIZE - p.size;
-            if (len > size)
-                len = size;
-            if (len > 0) {
-                update_page(p);
-#if NOT_DEFINED
-                p->data = realloc(p->data, p->size + len);
-                memmove(p->data + len, p->data, p->size);
-                memcpy(p->data, buf + size - len, len);
-                size -= len;
-                p->size += len;
-#endif
-            }
-        }
-
-        /* now add new pages if necessary */
-        n = (size + MAX_PAGE_SIZE - 1) / MAX_PAGE_SIZE;
-        if (n > 0) {
-#if NOT_DEFINED
-            for (int i=0; i<n; i++) {
-                page_table.Add(new Page());
-            }
-
-            b->nb_pages += n;
-            b->page_table = realloc(b->page_table, b->nb_pages * sizeof(Page));
-            p = b->page_table + page_index;
-            memmove(p + n, p,
-                    sizeof(Page) * (b->nb_pages - n - page_index));
-            while (size > 0) {
-                len = size;
-                if (len > MAX_PAGE_SIZE)
-                    len = MAX_PAGE_SIZE;
-                p->size = len;
-                p->data = malloc(len);
-                p->flags = 0;
-                memcpy(p->data, buf, len);
-                buf += len;
-                size -= len;
-                p++;
-            }
-#endif
-        }
-    }
-
-    /* We must have : 0 <= offset <= b->total_size */
-    void InsertLowlevel(int offset, byte[] buf, int size)
-    {
-        int len, len_out, page_index;
-        Page p = null;
-
-        total_size += size;
-
-#if NOT_ENABLED
-        /* find the correct page */
-        p = page_table[0];
-        if (offset > 0) {
-            offset--;
-            p = find_page(ref offset);
-            offset++;
-
-            /* compute what we can insert in current page */
-            len = MAX_PAGE_SIZE - offset;
-            if (len > size)
-                len = size;
-            /* number of bytes to put in next pages */
-            len_out = p.size + len - MAX_PAGE_SIZE;
-            page_index = PageIndex(p);
-            if (len_out > 0)
-                eb_insert1(b, page_index + 1, 
-                           p->data + p->size - len_out, len_out);
-            else
-                len_out = 0;
-
-            /* now we can insert in current page */
-            if (len > 0) {
-                p = page_table[page_index];
-                update_page(p);
-                int new_size = size + p.size + len - len_out;
-                byte[] new_data = new byte[new_size];
-                
-                p->data = realloc(p->data, p->size);
-                memmove(p->data + offset + len, 
-                        p->data + offset, p->size - (offset + len));
-                memcpy(p->data + offset, buf, len);
-                buf += len;
-                size -= len;
-            }
-        } else {
-            page_index = -1;
-        }
-#endif
-
-#if NOT_ENABLED
-        /* insert the remaining data in the next pages */
-        if (size > 0)
-            eb_insert1(b, page_index + 1, buf, size);
-#endif
-
-        /* the page cache is no longer valid */
-        cur_page = null;
-    }
-    
     /* Insert 'size' bytes from 'buf' into 'b' at offset 'offset'. We must
        have : 0 <= offset <= b->total_size */
     public void Insert(int offset, byte[] buf, int size)
     {
-
 //        eb_addlog(b, LOGOP_INSERT, offset, size);
-        InsertLowlevel(offset, buf, size);
-        /* the page cache is no longer valid */
-//        b->cur_page = NULL;
+        pages.InsertLowLevel(offset, buf, size);
     }
 }
